fda1484cbc9d196ead34b893332bb5ec10d64e0f
Olivir de Paula Junior <owlauch@hotmail.com>
2017-05-30 11:00:20 -0300

    0.0.8
    Organização de conteudo.

---
+++ ElementosSite/cabecalho.js
@@ -0,0 +1,30 @@
+function esccabecalho(n) {
+  if(n==1){
+  var $xy = document.querySelector('.teste'),
+  texto = '<img src="imagens/jake.gif" width="250px"  height="125px">',
+  t= texto+ '<img src="imagens/hypegif.gif" width="75px" height="75px" />',
+  t = t+ '<div class="icones">',
+  t = t+'<ul><li onclick="escassadores()">Assadores</li>',
+  t = t+'<li onclick="esbarmans()">Barmans</li>',
+  t = t+'<li onclick="escxt()">Caixas Termicas</li>',
+  t = t+'<li onclick="esks()">Kits Chop</li>',
+  t = t+'<li onclick="esdjs()">Djs</li>',
+  t = t+'<li onclick="essons()">Sons</li></ul>',
+  t = t+'</div><div class="ger"><ul><li><a href="ElementosSite/Cadastro.html">Login</a></li><li><a>Carrinho</a></li></ul></div>'
+  $xy.innerHTML=t;
+};
+  if(n==2){
+  var $xy = document.querySelector('.teste'),
+  texto = '<img src="../imagens/jake.gif" width="250px"  height="125px">',
+  t= texto+ '<img src="../imagens/hypegif.gif" width="75px" height="75px" />',
+  t = t+ '<div class="icones">',
+  t = t+'<ul><li onclick="escassadores()">Assadores</li>',
+  t = t+'<li onclick="esbarmans()">Barmans</li>',
+  t = t+'<li onclick="escxt()">Caixas Termicas</li>',
+  t = t+'<li onclick="esks()">Kits Chop</li>',
+  t = t+'<li onclick="esdjs()">Djs</li>',
+  t = t+'<li onclick="essons()">Sons</li></ul>',
+  t = t+'</div><div class="ger"><ul><li><a href="../ElementosSite/Cadastro.html">Login</a></li><li><a>Carrinho</a></li></ul></div>'
+  $xy.innerHTML=t;
+};
+};
